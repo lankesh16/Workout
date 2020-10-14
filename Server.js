286fb63e293887dd54799cf2d89a6d4c9c7a9e84
Lankesh <lankeshdesilva16@gmail.com>
2020-10-13 20:25:02 -0400

    push

---
+++ Server.js
@@ -0,0 +1,39 @@
+
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(morgan("dev"));
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+//var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+//mongoose.connect(MONGODB_URI, {
+  //  useNewUrlParser: true,
+    //useFindAndModify: false
+//})
+
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+
+app.listen(PORT, function(){
+    console.log(`Boom! listening on ${PORT}!`);
+});
---
+++ models/workout.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: () => new Date()
+    },
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise"
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Enter the name of the exercise"
+        },
+        duration: {
+          type: Number,
+          required: "Duration in Minutes"
+        },
+        weight: {
+          type: Number
+        },
+        reps: {
+          type: Number
+        },
+        sets: {
+          type: Number
+        },
+        distance: {
+          type: Number
+        }
+      }
+    ]
+  },
+  {
+    toJSON: {
+      
+      virtuals: true
+    }
+  }
+);
+
+
+workoutSchema.virtual("totalDuration").get(function () {
+  
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
+
+const Workout = mongoose.model("workout", workoutSchema);
+
+module.exports = Workout;
